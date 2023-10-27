b3b8ca31d6c7bae67c92f84d37cc115b365e7f93
Santiago Vallejo <santiagovallejoc@gmail.com>
2023-10-27 12:39:23 -0600

    precommit fix

--- pos/hotdog_stands/admin.py
+++ pos/hotdog_stands/admin.py
@@ -1,28 +1,35 @@
 from django.contrib import admin
-from pos.hotdog_stands.models import HotDogStand, InventoryItem, Inventory, Sale, SaleItem, Discount
+
+from pos.hotdog_stands.models import Discount, HotDogStand, Inventory, InventoryItem, Sale, SaleItem
+
 
 class InventoryInline(admin.TabularInline):
     model = Inventory
     extra = 1
-    fields = ['item', 'quantity', 'threshold']
+    fields = ["item", "quantity", "threshold"]
+
 
 class SaleItemInline(admin.TabularInline):
     model = SaleItem
     extra = 1
-    fields = ['inventory_item', 'quantity', 'price']
+    fields = ["inventory_item", "quantity", "price"]
 
+
+@admin.register(HotDogStand)
 class HotDogStandAdmin(admin.ModelAdmin):
-    list_display = ['id', 'operator', 'location']
+    list_display = ["id", "operator", "location"]
     inlines = [InventoryInline]
 
+
+@admin.register(Sale)
 class SaleAdmin(admin.ModelAdmin):
-    list_display = ['id', 'hotdog_stand', 'time']
+    list_display = ["id", "hotdog_stand", "time"]
     inlines = [SaleItemInline]
 
-class DiscountAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'percentage']
 
-admin.site.register(HotDogStand, HotDogStandAdmin)
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "percentage"]
+
+
 admin.site.register(InventoryItem)
-admin.site.register(Sale, SaleAdmin)
-admin.site.register(Discount, DiscountAdmin)
